9fe483b153772bde4af5deb30e5124581bfb5581
dungsil <opensource@younggeon.kim>
2023-06-04 18:58:19 +0900

    refactor: extract logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,8 @@
-import Logo from './Logo.tsx';
-
 export default function Navbar() {
   return (
     <header class='fixed w-full px-2 py-6 lg:px-8 shadow bg-zinc-950 bg-opacity-20 backdrop-filter backdrop-blur-sm'>
       <a href='#' class='font-black text-2xl text-white hover:text-gray-200'>
-        <Logo />
-        <span className='sr-only'>DUN LAND</span>
+        <img src='/logo.svg' class='w-5 h-5' alt='DUN LAND' />
       </a>
     </header>
   );
