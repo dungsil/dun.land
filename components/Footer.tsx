3adaa043ed119fd8e0f025370f6e17947df7a698
dungsil <opensource@younggeon.kim>
2023-06-04 18:58:51 +0900

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  const navigation = [
+    { name: '깃허브', href: 'https://github.com/dungsil/dun.land', icon: 'simple-icons:github' },
+  ];
+
+  return (
+    <footer class='bg-zinc-950'>
+      <div class='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
+        <div class='flex justify-center space-x-6 md:order-2'>
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} target='_blank' class='text-gray-400 hover:text-gray-500'>
+              <span class='sr-only'>{item.name}</span>
+              <iconify-icon icon={item.icon} class='h-6 w-6' />
+            </a>
+          ))}
+        </div>
+        <div class='mt-8 md:order-1 md:mt-0'>
+          <p class='text-center text-xs leading-5 text-gray-500'>
+            &copy; 2023 DUN LAND
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,20 @@
 import { Head } from '$fresh/runtime.ts';
 import { AppProps } from '$fresh/server.ts';
 import Navbar from '../components/Navbar.tsx';
+import Footer from '../components/Footer.tsx';
 
 export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
         <title>DUN</title>
+        <script src='https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js' defer></script>
       </Head>
       <div class='w-full min-h-screen bg-zinc-900 font-sans text-base text-gray-50'>
         <Navbar />
         <Component />
+
+        <Footer />
       </div>
     </>
   );
