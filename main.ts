12fb59ea1784284c4b5f4fe849bdd612ba1f8725
dungsil <opensource@younggeon.kim>
2023-06-05 15:40:21 +0900

    chore: add port env

--- main.ts
+++ main.ts
@@ -12,9 +12,12 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
+const port = Deno.env.has('DL_PORT') ? Number(Deno.env.get('DL_PORT')) : 8000;
+
 await start(
   manifest,
   {
+    port,
     plugins: [twindPlugin(twindConfig)],
     render: (ctx, render) => {
       ctx.lang = 'ko-KR'
